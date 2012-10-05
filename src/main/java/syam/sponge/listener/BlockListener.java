716075c1be043473117829e98f1639d16316cfe4
syamn <admin@sakura-server.net>
2012-10-06 02:25:21 +0900

    Add BlockFromToEvent event handler

--- src/main/java/syam/sponge/listener/BlockListener.java
+++ src/main/java/syam/sponge/listener/BlockListener.java
@@ -12,6 +12,7 @@ import org.bukkit.block.Block;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockFromToEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
 
 import syam.sponge.Sponge;
@@ -55,6 +56,36 @@ public class BlockListener implements Listener {
 					// 水か溶岩なら空気に変える
 					if (id == 8 || id == 9 || id == 10 || id == 11){
 						world.getBlockAt(x + cx, y + cy, z + cz).setTypeId(0);
+					}
+				}
+			}
+		}
+	}
+
+	// 流体ブロックの流れが変わった
+	@EventHandler(priority = EventPriority.HIGH, ignoreCancelled = true)
+	public void onBlockFromTo(final BlockFromToEvent event){
+		// 流体ブロック以外なら返す
+		int id = event.getBlock().getTypeId();
+		if (!(id == 8 || id == 9 || id == 10 || id == 11)){
+			return;
+		}
+
+		Block toBlock = event.getToBlock();
+		World world = toBlock.getWorld();
+		int radius = plugin.getConfigs().getRadius();
+
+		int x = toBlock.getX();
+		int y = toBlock.getY();
+		int z = toBlock.getZ();
+
+		for (int cx = -radius; cx <= radius; cx++){ // X軸走査
+			for (int cy = -radius; cy <= radius; cy++){ // Y軸走査
+				for (int cz = -radius; cz <= radius; cz++){ // Z軸走査
+					// ブロックIDチェック: 19(SPONGE)
+					if(world.getBlockTypeIdAt(x + cx, y + cy, z + cz) == 19){
+						event.setCancelled(true);
+						return;
 					}
 				}
 			}
